26a2c8ca9f994f1f3a40e04919f1034c73a07058
WhyRyan <105626562+WhyRyan@users.noreply.github.com>
2022-10-11 22:59:24 +0530

    1

---
+++ landing.js
@@ -0,0 +1,8 @@
+let text=document.getElementById("mt")
+let btn=document.getElementById("rs");
+let submit=document.getElementById("mp")
+btn.addEventListener("click",del);
+function del(){
+    text.value="";
+}
+//btn.addEventListener("click",sub);

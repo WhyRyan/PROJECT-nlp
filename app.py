0589e325d59c9a41c78b6f2211d8027ff3701fe1
WhyRyan <105626562+WhyRyan@users.noreply.github.com>
2022-10-27 05:01:20 +0530

    2

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
-   # return 'Hello, World!'
+@app.route('/postmethod',methods=['POST'])
+def get_post_javascript_data():
+    jsdata=request.form['javascript_data']
+    return jsdata
+
+
 
 if __name__== "__main__":
     app.run(debug=True,port=8000)
